0fc3d6c2338fb7a9874cd71e031b2e8cd55d00b1
Iryna Bibik <bibik.iryna004@gmail.com>
2024-08-17 14:05:36 +0300

    fetch api data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,99 @@
-import Navbar from "@/components/Navbar";
+'use client';
+
+import Navbar from '@/components/Navbar';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+interface WeatherData {
+	cod: string;
+	message: number;
+	cnt: number;
+	list: WeatherListItem[];
+	city: City;
+}
+
+interface WeatherListItem {
+	dt: number; // Unix timestamp
+	main: MainWeatherInfo;
+	weather: WeatherCondition[];
+	clouds: Clouds;
+	wind: Wind;
+	visibility: number;
+	pop: number; // Probability of precipitation
+	rain: Rain;
+	sys: System;
+	dt_txt: string; // Date and time of the forecast
+}
+
+interface MainWeatherInfo {
+	temp: number; // Temperature in Kelvin
+	feels_like: number; // Feels like temperature in Kelvin
+	temp_min: number; // Minimum temperature in Kelvin
+	temp_max: number; // Maximum temperature in Kelvin
+	pressure: number; // Atmospheric pressure in hPa
+	sea_level: number; // Atmospheric pressure at sea level in hPa
+	grnd_level: number; // Atmospheric pressure at ground level in hPa
+	humidity: number; // Humidity percentage
+	temp_kf: number; // Temperature coefficient
+}
+
+interface WeatherCondition {
+	id: number; // Weather condition id
+	main: string; // Group of weather parameters
+	description: string; // Weather condition description
+	icon: string; // Weather icon id
+}
+
+interface Clouds {
+	all: number; // Cloudiness percentage
+}
+
+interface Wind {
+	speed: number; // Wind speed in meter/sec
+	deg: number; // Wind direction in degrees
+	gust?: number; // Wind gust in meter/sec (optional)
+}
+
+interface Rain {
+	'3h': number; // Rain volume for the last 3 hours in mm
+}
+
+interface System {
+	pod: string; // Part of the day (day or night)
+}
+
+interface City {
+	id: number;
+	name: string;
+	coord: Coordinates;
+	country: string;
+	population: number;
+	timezone: number; // Timezone offset in seconds
+	sunrise: number; // Sunrise time in Unix timestamp
+	sunset: number; // Sunset time in Unix timestamp
+}
+
+interface Coordinates {
+	lat: number; // Latitude
+	lon: number; // Longitude
+}
 
 export default function Home() {
-  return (
-    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen"><Navbar/></div>
-  );
+	const { isLoading, error, data } = useQuery<WeatherData>(
+		'repoData',
+		async () => {
+			const { data } = await axios.get(
+				`https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`,
+			);
+			return data;
+		},
+	);
+
+  if (isLoading) return 'Loading...';
+  
+	return (
+		<div className='flex flex-col gap-4 bg-gray-100 min-h-screen'>
+			<Navbar />
+		</div>
+	);
 }
