d4a1b54c0c37316e92ce119e6fd125755dd80a24
Iryna Bibik <bibik.iryna004@gmail.com>
2024-08-18 19:49:02 +0300

    refactor

---
+++ src/components/SuggestionBox.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function SuggestionsBox({
+	showSuggestions,
+	suggestions,
+	handleSuggestionClick,
+	error,
+}: {
+	showSuggestions: boolean;
+	suggestions: string[];
+	handleSuggestionClick: (item: string) => void;
+	error: string;
+}) {
+	return (
+		<>
+			{((showSuggestions && suggestions.length > 1) || error) && (
+				<ul className='mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2'>
+					{error && suggestions.length < 1 && (
+						<li className='text-red-500 p-1'>{error}</li>
+					)}
+
+					{suggestions.map((el, index) => (
+						<li
+							className='cursor-pointer p-1 rounded hover:bg-gray-200'
+							key={index}
+							onClick={() => handleSuggestionClick(el)}
+						>
+							{el}
+						</li>
+					))}
+				</ul>
+			)}
+		</>
+	);
+}
